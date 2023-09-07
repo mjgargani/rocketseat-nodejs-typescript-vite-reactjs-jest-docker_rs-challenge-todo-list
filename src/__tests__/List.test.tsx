18d03689ecceedf4af2d3125571537461f3f71b4
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-09-07 16:53:15 -0300

    add jest

---
+++ src/__tests__/List.test.tsx
@@ -0,0 +1,11 @@
+// import { render } from "@testing-library/react"
+// import { List } from "../components/List"
+
+// import listMock from "./mock.json";
+
+describe("List test", () =>{
+  it("Return all list itens from mock", () =>{
+    // render(<List items={listMock}/>)
+    expect(true).toBe(true)
+  })
+})
